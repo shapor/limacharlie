f3893f008a3c9ef69508a4702471d00b1c07d084
Maxime Lamothe-Brassard <maximelb@google.com>
2016-03-11 11:56:47 -0800

    Batch update to prebuilt binaries for 0.9. Fixing mac kernel acquisition project for signing.

--- sensor/scripts/build_kernel_osx.sh
+++ sensor/scripts/build_kernel_osx.sh
@@ -3,5 +3,6 @@
 xcodebuild -project ./sensor/executables/hbs_kernel_acquisition/osx/hbs_kernel_acquisition.xcodeproj/
 
 mkdir -p ./sensor/bin/macosx/kernel
+rm -rf ./sensor/bin/macosx/kernel/*
 cp -R ./sensor/executables/hbs_kernel_acquisition/osx/build/Release/hbs_kernel_acquisition.kext ./sensor/bin/macosx/kernel/hbs_kernel_acquisition.kext
 
