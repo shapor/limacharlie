2568697a549b48977c1c260f73b540a79be93107
Maxime Lamothe-Brassard <maximelb@google.com>
2016-03-22 18:55:43 -0700

    Small fixes to the Hunters for spontaneous investigations.

--- cloud/beach/hcp/analytics/stateless/WinSuspExecLoc.py
+++ cloud/beach/hcp/analytics/stateless/WinSuspExecLoc.py
@@ -21,7 +21,8 @@ _xm_ = Actor.importLib( '../../hcp_helpers', '_xm_' )
 class WinSuspExecLoc ( StatelessActor ):
     def init( self, parameters ):
         super( WinSuspExecLoc, self ).init( parameters )
-        self.slocs = { 'tasks' : re.compile( r'.*windows\\(?:(?:system32)||(?:syswow64))\\tasks\\.*', re.IGNORECASE ),
+        self.slocs = { 'tasks' : re.compile( r'.*windows\\(?:(?:system32)||(?:syswow64))\\tasks\\.*',
+                                             re.IGNORECASE ),
                        'recycler' : re.compile( r'.*recycle.*', re.IGNORECASE ),
                        'fonts' : re.compile( r'.*\\windows\\fonts\\.*', re.IGNORECASE ),
                        'help' : re.compile( r'.*\\windows\\help\\.*', re.IGNORECASE ),
