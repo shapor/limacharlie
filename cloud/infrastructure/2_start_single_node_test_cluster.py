8607a2a575ced9e0e19be94bbf46e04de23ecb28
Maxime Lamothe-Brassard <maximelb@google.com>
2016-06-02 20:23:01 -0700

    Moving the REST interface to the new builtin Beach REST bridge giving access via REST to everything.

--- cloud/infrastructure/2_start_single_node_test_cluster.py
+++ cloud/infrastructure/2_start_single_node_test_cluster.py
@@ -79,12 +79,8 @@ printStep( 'Starting the HTTP endpoint for the cloud (in a screen).',
                                                             'beach',
                                                             'sample_cluster.yaml' ) ) ) )
 
-printStep( 'Starting the LIMA CHARLIE REST interface on port 8000 (in a screen).',
-    os.system( 'screen -d -m python %s %s 8000'% ( os.path.join( root,
-                                                                 'cloud',
-                                                                 'beach',
-                                                                 'rest_endpoint.py' ),
-                                                   os.path.join( root,
-                                                                 'cloud',
-                                                                 'beach',
-                                                                 'sample_cluster.yaml' ) ) ) )
+printStep( 'Starting the BEACH REST interface on port 8000 (in a screen).',
+    os.system( 'screen -d -m python -m beach.restbridge 8000 %s hcp'% ( os.path.join( root,
+                                                                                      'cloud',
+                                                                                      'beach',
+                                                                                      'sample_cluster.yaml' ), ) ) )
